43d7cf772e8a0064e6c40515b803dfdab25669d5
Christoph Bühler <christoph@cbuehler.li>
2020-11-03 00:54:17 +0100

    centered content

--- page.php
+++ page.php
@@ -6,22 +6,26 @@
 
 get_header();
 
-if (have_posts()) :
-    while (have_posts()) :
-        the_post();
-        get_template_part('template-parts/content', get_post_type());
-
-    endwhile;
-else :
-    echo 'Here should be a page.';
-endif;
 ?>
+<div class="content">
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            get_template_part('template-parts/content', get_post_type());
 
-<div class="link-generator">
-    <div class="title">link generator</div>
-    <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
-    <input type="text">
-    <button>Download Big Blue Button Video</button>
+        endwhile;
+    else :
+        echo 'Here should be a page.';
+    endif;
+    ?>
+
+    <div class="link-generator">
+        <div class="title">link generator</div>
+        <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
+        <input type="text">
+        <button>Download Big Blue Button Video</button>
+    </div>
 </div>
 
 <?php
