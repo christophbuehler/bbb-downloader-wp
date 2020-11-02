32e5b49bbd3c269040b7def31db70b91677f8271
Christoph Bühler <christoph@cbuehler.li>
2020-11-03 00:47:05 +0100

    some hacking

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 get_header();
-if (have_posts()) :
+/*if (have_posts()) :
     while (have_posts()) :
         the_post();
         get_template_part('template-parts/content', get_post_type());
 
     endwhile;
-endif;
+endif;*/
 ?>
+
+<div class="link-generator">
+    <div class="title">link generator</div>
+    <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
+    <input type="text">
+    <button>Download Big Blue Button Video</button>
+</div>
+
 <?php wp_footer(); ?>
 
 <script>
