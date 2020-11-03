f07d03923061dd06567acca4cfbd1b415e4dbaab
Christoph Bühler <christoph@cbuehler.li>
2020-11-03 01:23:04 +0100

    functionality test

--- page.php
+++ page.php
@@ -25,6 +25,7 @@ get_header();
         <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
         <input type="text" placeholder="Enter Big Blue Button Link">
         <a href="#" download class="btn" disabled>Download Big Blue Button Video</a>
+        <div class="error"></div>
     </div>
 </div>
 
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ export default () => {
 };
 
 function ready() {
-  const meetingLink = 'https://bbb-scale.univie.ac.at/playback/presentation/2.0/playback.html?meetingId=ab96390226077858e4976a070069d6b1b73f7ff4-1604297356406';
-  const downloadLink = generateBbbDownloadLink(meetingLink);
+  // const meetingLink = 'https://bbb-scale.univie.ac.at/playback/presentation/2.0/playback.html?meetingId=ab96390226077858e4976a070069d6b1b73f7ff4-1604297356406';
+  // const downloadLink = generateBbbDownloadLink(meetingLink);
 
   const genDiv = document.querySelector('.link-generator') as HTMLDivElement;
   const errDiv = document.querySelector('.error') as HTMLDivElement;
@@ -34,7 +34,7 @@ function ready() {
       const downloadLink = generateBbbDownloadLink(link);
       downloadBtn.href = downloadLink;
     } catch(e) {
-      showError(e);
+      showError(e.message);
       return;
     }
   
@@ -52,6 +52,7 @@ function ready() {
   }
   
   function showError(err: string) {
+    errDiv.innerHTML = err;
     errDiv.style.display = 'block';
   }
 }
@@ -60,5 +61,18 @@ function generateBbbDownloadLink(link: string) {
   const domain = link.split('/playback/')[0];
   const title = link.split('/playback/')[1].split('/')[0];
   const meetingId = link.split('meetingId=')[1];
+
+  if (domain === '') {
+    throw new Error('The link does not contain a domain.');
+  }
+
+  if (title === '') {
+    throw new Error('The link does not contain a title.');
+  }
+
+  if (meetingId === '') {
+    throw new Error('The link does not contain a meeting id.');
+  }
+
   return `${domain}/${title}/${meetingId}/video/webcams.webm`;
 }
