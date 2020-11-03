c799cf0270ab8ee0149e62f1995b19a86a05678f
Christoph Bühler <christoph@cbuehler.li>
2020-11-03 01:43:48 +0100

    download button design

--- page.php
+++ page.php
@@ -24,7 +24,7 @@ get_header();
         <div class="title">Download Big Blue Button Video</div>
         <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
         <input type="text" placeholder="Enter Big Blue Button Link...">
-        <a href="#" download class="btn" disabled>Download Big Blue Button Video</a>
+        <a href="#" download="webcams.webm" class="btn" disabled>Download Big Blue Button Video</a>
         <div class="error"></div>
     </div>
 </div>
