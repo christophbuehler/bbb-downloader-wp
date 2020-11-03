b2ad55437c0eb1c464d9bd743bc1efcd12b7f260
Christoph Bühler <christoph@cbuehler.li>
2020-11-03 01:30:05 +0100

    colors

--- page.php
+++ page.php
@@ -21,7 +21,7 @@ get_header();
     ?>
 
     <div class="link-generator">
-        <div class="title">link generator</div>
+        <div class="title">Download Big Blue Button Video</div>
         <div class="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum dolore veritatis modi quas voluptates vitae aspernatur doloribus eveniet exercitationem, ullam repudiandae, labore non culpa quisquam quibusdam magni, nemo aliquam.</div>
         <input type="text" placeholder="Enter Big Blue Button Link">
         <a href="#" download class="btn" disabled>Download Big Blue Button Video</a>
